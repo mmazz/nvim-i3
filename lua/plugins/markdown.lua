5177f2010fb9928a7f151f6f34ea8b172e624a17
Matias Mazzanti <mazzantimatiass@gmail.com>
2026-04-08 10:29:53 -0300

    markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,17 @@
--- lua/plugins/markdown.lua
 return {
   "iamcco/markdown-preview.nvim",
-  build = "cd app && npm install",
   ft = { "markdown" },
-}
+  build = "cd app && npm install",
+  config = function()
+    -- Configuración básica
+    vim.g.mkdp_auto_start = 0
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_browser = "" -- usa navegador por defecto
+    vim.g.mkdp_echo_preview_url = 1
 
+    -- Keymaps
+    vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<CR>", { desc = "Markdown Preview" })
+    vim.keymap.set("n", "<leader>ms", "<cmd>MarkdownPreviewStop<CR>", { desc = "Stop Preview" })
+    vim.keymap.set("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Toggle Preview" })
+  end,
+}
